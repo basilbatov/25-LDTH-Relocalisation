88b854f5bde2aa5eece67c4409432ed5a14c8438
Basil Batov <basil.batov@gmail.com>
2025-05-10 17:59:40 +0100

    Added function for get_satellite_image

--- src/utility/get_satellite_image.py
+++ src/utility/get_satellite_image.py
@@ -4,44 +4,37 @@ import requests
 from io import BytesIO
 import matplotlib.pyplot as plt
 from src.utility.bounding_box import BoundingBox, Projection
-
-# High res 2021 satellite imagery from Copernicus
-# url = "https://image.discomap.eea.europa.eu/arcgis/rest/services/GioLand/VHR_2021_LAEA/ImageServer/exportImage"
-# High res 2018 satellite imagery from Copernicus
-url = "https://image.discomap.eea.europa.eu/arcgis/rest/services/GioLand/VHR_2018_LAEA/ImageServer/exportImage"
-
-example_bbox = BoundingBox(
-    min_lat=3220365,
-    max_lat=3226152,
-    min_lon=3594402,
-    max_lon=3602683,
-    projection=Projection.EPSG_3035
-)
-
-# Bounding box is in EPSG:3035 (since it's LAEA projection)
-params = {
-    "bbox": example_bbox.to_query_string(),
-    "bboxSR": "3035",
-    "size": "512,512",
-    # "size": "1024,1024",
-    # "size": "2048,2048",
-    # "size": "5096,5096",
-    # "size": "10192,10192", 
-    "imageSR": "3035",
-    "format": "png",  # can also be "tiff"
-    "f": "image",
-}
-
-response = requests.get(url, params=params)
-
-try:
-    response.raise_for_status()
-    image = cv2.imdecode(np.frombuffer(response.content, np.uint8), cv2.IMREAD_UNCHANGED)
-    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB)) # type: ignore
-    plt.axis('off') # type: ignore
-    plt.show() # type: ignore
-
-except requests.exceptions.HTTPError as err:
-    print(f"HTTP error occurred: {err}")
-    print("Response content:", response.content)
     
+def get_satellite_image(bbox: BoundingBox) -> np.ndarray:
+    # High res 2021 satellite imagery from Copernicus
+    # url = "https://image.discomap.eea.europa.eu/arcgis/rest/services/GioLand/VHR_2021_LAEA/ImageServer/exportImage"
+    # High res 2018 satellite imagery from Copernicus
+    url = "https://image.discomap.eea.europa.eu/arcgis/rest/services/GioLand/VHR_2018_LAEA/ImageServer/exportImage"
+
+    # Bounding box is in EPSG:3035 (since it's LAEA projection)
+    params = {
+        "bbox": bbox.to_query_string(),
+        "bboxSR": "3035",
+        "size": "512,512",
+        # "size": "1024,1024",
+        # "size": "2048,2048",
+        # "size": "5096,5096",
+        # "size": "10192,10192", 
+        "imageSR": "3035",
+        "format": "png",  # can also be "tiff"
+        "f": "image",
+    }
+
+    response = requests.get(url, params=params)
+
+    try:
+        response.raise_for_status()
+        image = cv2.imdecode(np.frombuffer(response.content, np.uint8), cv2.IMREAD_UNCHANGED)
+        plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB)) # type: ignore
+        plt.axis('off') # type: ignore
+        plt.show() # type: ignore
+    except requests.exceptions.HTTPError as err:
+        print(f"HTTP error occurred: {err}")
+        print("Response content:", response.content)
+    
+    return image
